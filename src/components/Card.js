8da59ba583dda82d4fbb88444ee0017b187ffaa5
hyf <rufaidamohammadjumaa@gmail.com>
2021-01-22 03:35:34 +0100

    Add card component

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Card extends React.Component{
+    render(){
+        return(
+            <div className="github-profile">The Card ...</div>
+        )
+    }
+}
+
+export default Card;
