fc0de68dd55683ffd4012753ca5b86b0930157b1
hyf <rufaidamohammadjumaa@gmail.com>
2021-01-24 22:47:25 +0100

    Delete image word from alt

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ class Card extends React.Component{
     render(){
         return(
             <div className="github-profile">
-                <img src={this.props.avatar_url} alt="Profile image"/>
+                <img src={this.props.avatar_url} alt="Profile"/>
                 <div className="info">
                     <div className="name">{this.props.name}</div>
                     <div className="company">{this.props.company}</div>
