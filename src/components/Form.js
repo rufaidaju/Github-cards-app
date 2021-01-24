2191bfe7a78e7a7f45a44e73b50fa00b1ac043ec
hyf <rufaidamohammadjumaa@gmail.com>
2021-01-24 22:39:37 +0100

    Reset input field after submitting

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,7 +11,8 @@ class Form extends React.Component {
         console.log(this.state.userName);
         const response = await axios.get(`https://api.github.com/users/${this.state.userName}`);
         const profile = response.data;
-        this.props.onSubmit(profile)
+        this.props.onSubmit(profile);
+        this.setState({userName :''})
        
     }
     render() {
