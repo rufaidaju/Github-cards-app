03e8abb19663035875f9812c8dc46d288f3e47db
hyf <rufaidamohammadjumaa@gmail.com>
2021-01-24 01:16:39 +0100

    Pass the info from cardslist to card as props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,13 @@
 import React from 'react';
 
-const testData = [
-    {
-    name: "Tom Preston-Werner",
-    avatar_url:"https://avatars.githubusercontent.com/u/2?v=4",
-    company:"@chatterbugapp, @redwood…reston-werner-ventures "
-    },
-    {
-        name: "PJ",
-        avatar_url:"https://avatars.githubusercontent.com/u/3?v=4",
-        company:"@Github "
-        }
-
-];
-
 class Card extends React.Component{
     render(){
         return(
-            <div className="github-profile" style={{backgroundColor:'red'}}>
-                <img src={testData[0].avatar_url} alt="Profile image"/>
+            <div className="github-profile">
+                <img src={this.props.avatar_url} alt="Profile image"/>
                 <div className="info">
-                    <div className="name">{'name'}</div>
-                    <div className="company">{'Company'}</div>
+                    <div className="name">{this.props.name}</div>
+                    <div className="company">{this.props.company}</div>
                 </div>
             </div>
         )
--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -2,11 +2,24 @@ import React from 'react';
 import Card from './Card'
 
 
+const testData = [
+    {
+    name: "Tom Preston-Werner",
+    avatar_url:"https://avatars.githubusercontent.com/u/2?v=4",
+    company:"@chatterbugapp, @redwood…reston-werner-ventures "
+    },
+    {
+        name: "PJ",
+        avatar_url:"https://avatars.githubusercontent.com/u/3?v=4",
+        company:"@Github "
+        }
+];
+
 function CardsList(){
     return(
         <div>
-            <Card />
-            <Card />
+            <Card {...testData[0]}/>
+            <Card {...testData[1]}/>
         </div>
     )
 }
