2d3946b94e5b6cdcbe5efd6949990bc1f526b7ed
hyf <rufaidamohammadjumaa@gmail.com>
2021-01-24 16:27:45 +0100

    Add form to insert user github name

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Form extends React.Component {
+    render (){
+        return(
+            <form action="">
+                <input type="text" placeholder="Gitub user name" />
+                <button>Add Card</button>
+            </form>
+        )
+    }
+}
+export default Form;
