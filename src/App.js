68cc75063cd3c83cd27df5e46260e840f00ebeb5
hyf <rufaidamohammadjumaa@gmail.com>
2021-01-24 01:54:07 +0100

    Move the testData array from CardsList to App.js and pass info as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import CardsList from './components/CardsList'
+
+const testData = [
+  {
+  name: "Tom Preston-Werner",
+  avatar_url:"https://avatars.githubusercontent.com/u/2?v=4",
+  company:"@chatterbugapp, @redwood…reston-werner-ventures "
+  },
+  {
+      name: "PJ",
+      avatar_url:"https://avatars.githubusercontent.com/u/3?v=4",
+      company:"@Github "
+  },
+  {
+      name: "Ryu kde",
+      avatar_url:"https://avatars.githubusercontent.com/u/38?v=4",
+      company:"@chatreston-werneures "
+  }    
+];
 
 class App extends React.Component{
   render(){
     return(
+    <div>
       <div className="header">{this.props.title}</div>
+      <CardsList profiles={testData}/>
+    </div>
     )
   }
 }
--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,29 +1,10 @@
 import React from 'react';
 import Card from './Card'
 
-
-const testData = [
-    {
-    name: "Tom Preston-Werner",
-    avatar_url:"https://avatars.githubusercontent.com/u/2?v=4",
-    company:"@chatterbugapp, @redwood…reston-werner-ventures "
-    },
-    {
-        name: "PJ",
-        avatar_url:"https://avatars.githubusercontent.com/u/3?v=4",
-        company:"@Github "
-    },
-    {
-        name: "Ryu kde",
-        avatar_url:"https://avatars.githubusercontent.com/u/38?v=4",
-        company:"@chatreston-werneures "
-    }    
-];
-
-function CardsList(){
+function CardsList(props){
     return(
         <div>
-            { testData.map(profile => <Card {...profile} /> )}
+            { props.profiles.map(profile => <Card {...profile} /> )}
         </div>
     )
 }
