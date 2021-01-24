7da9f992948ce7ccb7412c14addf9c925bf7dd65
hyf <rufaidamohammadjumaa@gmail.com>
2021-01-24 01:12:03 +0100

    Add CardsList component to show many profiles

---
+++ src/components/CardsList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card'
+
+
+function CardsList(){
+    return(
+        <div>
+            <Card />
+            <Card />
+        </div>
+    )
+}
+
+export default CardsList;
